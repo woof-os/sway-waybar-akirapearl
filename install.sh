2f54bb6b145e2b82a04b5494c86528a95504d1e0
Akirapearl <draculaspot@hotmail.com>
2024-05-14 22:46:27 +0200

    Improved install script, move starship toml under config directory

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ echo "This is a script to install my dotfiles."
 echo "Type the letter for the corresponding software you want to install"
 echo "============================================================="
 echo "a) Starship"
+echo "b) Alacritty (requirements, NOT package itself)"
 
 read install
 
@@ -14,6 +15,11 @@ if [ $install == "a" ]; then
 	sleep 0.5
 	curl -sS https://starship.rs/install.sh | sh
 	echo  'eval "$(starship init bash)"' > ~/.bashrc
+	config=/home/$USER/.config/
+	mkdir $config
+	cp starship.toml $config
+
+elif [ $install == "b" ]; then
 	echo "============================"
 	echo "Installing required nerd fonts..."
 	echo "============================"
@@ -33,4 +39,5 @@ if [ $install == "a" ]; then
 	sleep 0.5
 	mkdir /home/$USER/.config/alacritty/ 
 	cp -r /home/$USER/.config/alacritty/ 
-fi
+
+	fi
